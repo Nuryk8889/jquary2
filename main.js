cfd7228d5b9779cfa559e660e3db0d0b05aca8fb
Nuryk8889 <tashpolotovnurjigit8@gmail.com>
2023-02-08 15:28:05 +0600

    fles

---
+++ main.js
@@ -0,0 +1,34 @@
+
+// function f() {
+//     function f1() {
+//     $('.img1').css({"opacity":"1"})
+//     $('.img6').css({"opacity":"0"})
+// }
+// setTimeout(f1, 1000)
+// function f2() {
+//     $('.img2').css({"opacity":"1"})
+//     $('.img1').css({"opacity":"0"})
+// }
+// setTimeout(f2, 2000)
+// function f3() {
+//     $('.img3').css({"opacity":"1"})
+//     $('.img2').css({"opacity":"0"})
+// }
+// setTimeout(f3, 3000)
+// function f4() {
+//     $('.img4').css({"opacity":"1"})
+//     $('.img3').css({"opacity":"0"})
+// }
+// setTimeout(f4, 4000)
+// function f5() {
+//     $('.img5').css({"opacity":"1"})
+//     $('.img4').css({"opacity":"0"})
+// }
+// setTimeout(f5, 5000)
+// function f6() {
+//     $('.img6').css({"opacity":"1"})
+//     $('.img5').css({"opacity":"0"})
+// }
+// setTimeout(f6, 6000)
+// }
+// setInterval(f, 6100)
